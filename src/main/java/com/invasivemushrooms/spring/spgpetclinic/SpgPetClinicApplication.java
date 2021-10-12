621539b55102c9f16f3cbcfab609aedbadb7faf4
= <=>
2021-10-12 21:00:34 +0200

    initial commit

---
+++ src/main/java/com/invasivemushrooms/spring/spgpetclinic/SpgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.invasivemushrooms.spring.spgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpgPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpgPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/invasivemushrooms/spring/spgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invasivemushrooms.spring.spgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
